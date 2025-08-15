b33f2716cf436547055274d45b6e33a215c40fcf
Krishnapriya Saravanakumar <149654594+Krishna010804priya@users.noreply.github.com>
2025-08-15 11:52:25 +0530

    Update SocketContext.jsx

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://trio-chat-app-1.onrender.com/", {
+			const socket = io("https://trio-chat-app-new.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
